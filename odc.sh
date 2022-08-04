10cfa3cbfec1aaf5526aecc931384c2ce1bab6af
Admane Ajay <105783441+AjayRaj971015@users.noreply.github.com>
2022-08-04 15:26:51 +0530

    Create odc.sh

---
+++ odc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget "https://raw.githubusercontent.com/AjayRaj971015/devsecops/master/dc.sh"
+chmod +x dc.sh
+./dc.sh
+echo "Reports of OWASP-Dependancy-check"
+ls -l odc-reports/
+echo "Displaying Reports on Apache webpage "
+cp odc-reports/dependency-check-report.html /var/www/html/
+systemctl restart apache2
